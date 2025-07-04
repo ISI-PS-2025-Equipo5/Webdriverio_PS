53e2856488d7eb56b7cb9be38a88796ff88b1e2d
YennyE <44005339+YennyE@users.noreply.github.com>
2025-07-04 13:07:54 -0500

    Create accountOverviewSteps.js

---
+++ features/step-definitions/accountOverviewSteps.js
@@ -0,0 +1,22 @@
+import { Given, Then } from '@wdio/cucumber-framework';
+import AccountsOverviewPage from '../pageobjects/accountsOverview.page.js';
+import { sleep } from "../pageobjects/page.js";
+
+
+Given(/^I am on the accounts overview page$/, async () => {
+  await AccountsOverviewPage.open(); 
+});
+
+Then(/^I should see a list of all my accounts$/, async () => {
+  const rows = await AccountsOverviewPage.rows;
+  await expect(rows.length).toBeGreaterThan(0);
+});
+
+Then(/^each account should display the current balance$/, async () => {
+  const balances = await AccountsOverviewPage.getBalances();
+
+  for (const balance of balances) {
+    await expect(balance).not.toBe('');
+    await expect(balance).toMatch(/^\$?[0-9,]+(\.\d{2})?$/); // formato $1,000.00
+  }
+});
